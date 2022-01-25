24190909ff38db02696a7c9c7f7130ca8599ff49
qirong.zhang <qirong.zhang@za.group>
2022-01-25 11:02:05 +0800

    IU-2021.3.1 <qirong.zhang@NB-SH-031 Create baseRefactoring.xml, ide.general.xml, git_toolbox_2.xml, project.default.xml, rainbow_brackets.xml, advancedSettings.xml, Interface.java, lombok-plugin.xml, user.xml, yiiguxing.translation.xml, Default.xml, find.xml, ignored-suggested-plugins.xml, ui.lnf.xml, filetypes.xml, projectView.xml, colors.scheme.xml, Myself.xml, KeyPromoterXSettings.xml, databaseDrivers.xml, Copyright.java, templates.xml, javarebel.xml, laf.xml, Default.xml, studioFlags.xml, Class.java, jdk.table.xml, gitee.xml, chinese_completion_helper.xml, vcs.xml, codota.xml, mybatis.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,10 @@
+#parse("Copyright.java")
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * @author qirong.zhang
+ * @since ${DATE} ${TIME}
+ */
+public interface ${NAME} {
+}
